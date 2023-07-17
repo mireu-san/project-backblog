b2732de9d3ffc7dd5bc66fb9fca9f1de8dcf30ac
mireu-san <starmireu@gmail.com>
2023-07-17 15:54:37 +0900

    updating - templates

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -5,6 +5,6 @@ app_name = "blog"
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blog/', views.blog_list, name='blog_list'),
-    path('blog/<int:pk>', views.blog_detail, name='blog_detail'),
+    path('list/', views.blog_list, name='blog_list'),
+    path('<int:pk>/', views.blog_detail, name='blog_detail'),
 ]
