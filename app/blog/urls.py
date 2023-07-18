47613acf779f7bbf688f38afd2dd753fbf06bbb7
mireu-san <starmireu@gmail.com>
2023-07-18 11:39:06 +0900

    backup - urls issue

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -3,8 +3,13 @@ from . import views
 
 app_name = "blog"
 
+"""
+/ 또는 /blog/ 로 접속했을 때, views.py 에서 home 함수를 실행하도록 설정
+
+"""
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('list/', views.blog_list, name='blog_list'),
+    path('blog/', views.blog_page, name='blog_page'),
     path('<int:pk>/', views.blog_detail, name='blog_detail'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -3,8 +3,13 @@ from django.http import HttpResponse
 from .models import Post
 
 def home(request):
+    posts = Post.objects.all()
     # 페이지 제목과 블로그 입장하기 버튼을 렌더링
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'posts': posts})
+
+def blog_page(request):
+    posts = Post.objects.all()
+    return render(request, 'blog.html', {'posts': posts})
 
 def blog_list(request):
     posts = Post.objects.all()
