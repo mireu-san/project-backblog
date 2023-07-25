86207c64bd1a8be4e2dd1039a1cb7cd176399833
mireu-san <starmireu@gmail.com>
2023-07-25 17:13:29 +0900

    makemigrations & admin.py

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
 
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
